221497512b9872c990881065f362311a3ff88007
naveo <naa.veoos@gmail.com>
2025-04-08 21:35:08 +0200

    Ajout de variables d'environnement pour des assertions

--- src/core/password_generator.py
+++ src/core/password_generator.py
@@ -1,29 +1,25 @@
 from secrets import choice
 from string import ascii_letters, digits, punctuation
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+PASSWORD_MIN_SIZE = getenv("PASSWORD_MIN_SIZE")
+PASSWORD_MAX_SIZE = getenv("PASSWORD_MAX_SIZE")
 
 def build_readable_password(length:int):
-    """Créé un mot de passe composé d'une alternance de voyelles et de consonnes.
-
-    Args:
-        length (int): La longueur du mot de passe.
-
-    Returns:
-        str: Le mot de passe généré.
-    """
+    assert length >= PASSWORD_MIN_SIZE
+    assert length <= PASSWORD_MAX_SIZE
     vowels = "aeiouyAEIOUY"
     consonants = ''.join([c for c in ascii_letters if c not in vowels])
     return ''.join(choice(vowels) if (i % 2 == 0) else choice(consonants)for i in range(length))
 
 def build_random_password(length:int, character_flags:list):
-    """Créé un mot de passe composé de caractères spécifiés par la liste character_flag passée en argument.
-
-    Args:
-        length (int): La longueur du mot de passe.
-        character_flags (list): Les charactères à ajouter ou non définis par un booléen([Lettres ASCII, Nombres, Caractères spéciaux]).
-
-    Returns:
-        str: Le mot de passe généré.
-    """
+    assert length >= PASSWORD_MIN_SIZE
+    assert length <= PASSWORD_MAX_SIZE
+    assert len(character_flags) == 3
+    for i in range(len(character_flags)):
+        assert type(character_flags[i]) == bool
     pool = ""
     if (character_flags[0]):
         pool += ascii_letters
@@ -33,15 +29,10 @@ def build_random_password(length:int, character_flags:list):
         pool += punctuation
     return ''.join(choice(pool) for _ in range(length))
 
-def generate_password(length: int, characters: list, readable: bool):
-    """Créé un mot de passe.
-
-    Args:
-        length (int): Longueur du mot de passe.
-        characters (list): Les charactères à ajouter ou non définis par un booléen([Lettres ASCII, Nombres, Caractères spéciaux]).
-        readable (bool): Lisibilité du mot de passe généré.
-
-    Returns:
-        str: Mot de passe généré.
-    """
-    return build_readable_password(length) if (readable) else build_random_password(length, characters)
+def generate_password(length:int=PASSWORD_MIN_SIZE, character_flags:list=[True,True,True], readable:bool=False):
+    assert length >= PASSWORD_MIN_SIZE
+    assert length <= PASSWORD_MAX_SIZE
+    assert len(character_flags) == 3
+    for i in range(len(character_flags)):
+        assert type(character_flags[i]) == bool
+    return build_readable_password(length) if (readable) else build_random_password(length, character_flags)
